294241539157f2468f16118da42ca458dc880ea4
SarahStanton <stants5@uw.edu>
2016-08-14 00:58:59 +0000

    login

--- instaSite/urls.py
+++ instaSite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 	url(r'^$', views.category_list, name='category_list'),
@@ -7,5 +8,7 @@ urlpatterns = [
 	url(r'^location$', views.location, name='location'),
 	url(r'^picture$', views.picture_list, name='picture_list'),
 	url(r'^category/new/$', views.category_new, name="category_new"),
-	# url(r'^category/(?P<category_name_url>\w+)/$', views.picture_list, name='picture_list'),
+	url(r'^accounts/login/$', auth_views.login, name='login'),
+	url(r'^accounts/logout/$', views.logout_view, name='logout'),
+	
 ]
--- instaSite/views.py
+++ instaSite/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import redirect, render
 from .models import Picture, Category
 from .forms import CategoryForm
 import requests
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import views as auth_views
 
 # Create your views here.
+@login_required(login_url='accounts/login/')
 def category_list(request):
 	categories = Category.objects.all()
 	return render(request, 'instaSite/category_list.html', {'categories': categories})
 
+@login_required(login_url='accounts/login/')
 def category_new(request):
 	if request.method == "POST":
 		form = CategoryForm(request.POST)
@@ -20,6 +25,7 @@ def category_new(request):
 		form = CategoryForm()
 	return render(request, 'instaSite/category_new.html', {'form': form})
 
+@login_required(login_url='accounts/login/')
 def picture_list(request):
 	"""
 	category_name = category_url.replace(category_url, '_', ' ')
@@ -35,9 +41,16 @@ def picture_list(request):
 	pictures = Picture.objects.filter(category=2)
 	return render(request, 'instaSite/picture_list.html', {'pictures': pictures})
 
+@login_required(login_url='accounts/login/')
 def hashtag(request):
     return render(request, 'instaSite/hashtag.html', {})
 
+@login_required(login_url='accounts/login/')
 def location(request):
     return render(request, 'instaSite/location.html', {})
+
+def logout_view(request):
+	logout(request)
+	return redirect('login')
+
 
