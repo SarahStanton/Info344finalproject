1acac41cb2e422c071d7b7cf91c970c70d7f662f
Rachel Paresa <rparesa@uw.edu>
2016-08-16 15:15:00 +0000

    oauth

--- instaSite/views.py
+++ instaSite/views.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import, print_function
 from django.shortcuts import redirect, render
 from .models import Picture, Category
 from .forms import CategoryForm
@@ -5,16 +6,76 @@ import requests
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth import views as auth_views
-import flickr_api
-from rauth import OAuth1Service
+from django.conf import settings
+from django.http import HttpResponseRedirect, HttpResponse
+
+CONSUMER_TOKEN=
+CONSUMER_SECRET=
+'''CALLBACK_URL = set it in my twitter acc to link to http://rparesa.info344.com/ ? '''
+
+import tweepy
+app.route("/")
+def send_token():
+	auth = tweepy.OAuthHandler(CONSUMER_TOKEN, 
+		CONSUMER_SECRET, 
+		CALLBACK_URL)
+
+	try: 
+		#get the request tokens
+		redirect_url= auth.get_authorization_url()
+		session['request_token']= (auth.request_token.key,
+			auth.request_token.secret)
+	except tweepy.TweepError:
+		print 'Error! Failed to get request token'
+
+	#this is twitter's url for authentication
+	return flask.redirect(redirect_url)	
+
+@app.route("/verify")
+def get_verification():
+
+	#get the verifier key from the request url
+	verifier= request.args['oauth_verifier']
+
+	auth = tweepy.OAuthHandler(CONSUMER_TOKEN, CONSUMER_SECRET)
+	token = session['request_token']
+	del session['request_token']
+
+	auth.set_request_token(token[0], token[1])
+
+	try:
+		    auth.get_access_token(verifier)
+	except tweepy.TweepError:
+		    print 'Error! Failed to get access token.'
+
+	#now you have access!
+	api = tweepy.API(auth)
+
+	#store in a db
+	db['api']=api
+	db['access_token_key']=auth.access_token.key
+	db['access_token_secret']=auth.access_token.secret
+	return flask.redirect(flask.url_for('start'))
+
+@app.route("/start")
+def start():
+	#auth done, app logic can begin
+	api = db['api']
+
+	#example, print your latest status posts
+	return flask.render_template('tweets.html', tweets=api.user_timeline())
+
+if __name__ == "__main__":
+	app.run()
+view raw
 
 # Create your views here.
-@login_required(login_url='accounts/login/')
+
 def category_list(request):
 	categories = Category.objects.all()
 	return render(request, 'instaSite/category_list.html', {'categories': categories})
 
-@login_required(login_url='accounts/login/')
+
 def category_new(request):
 	if request.method == "POST":
 		form = CategoryForm(request.POST)
@@ -27,7 +88,7 @@ def category_new(request):
 		form = CategoryForm()
 	return render(request, 'instaSite/category_new.html', {'form': form})
 
-@login_required(login_url='accounts/login/')
+
 def picture_list(request):
 	"""
 	category_name = category_url.replace(category_url, '_', ' ')
@@ -43,19 +104,19 @@ def picture_list(request):
 	pictures = Picture.objects.filter(category=2)
 	return render(request, 'instaSite/picture_list.html', {'pictures': pictures})
 
-@login_required(login_url='accounts/login/')
+
 def hashtag(request):
     return render(request, 'instaSite/hashtag.html', {})
 
-@login_required(login_url='accounts/login/')
 def location(request):
-    return render(request, 'instaSite/location.html', {})
+	return render(request, 'instaSite/location.html', {})
+
 
 def logout_view(request):
 	logout(request)
 	return redirect('login')
-
+'''
 def login_view():
 	a = flickr_api.auth.AuthHandler()
-
+'''
 
