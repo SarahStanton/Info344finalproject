022ff311f19395e94379b01b14b0562ef6ffbc02
Rachel Paresa <rparesa@uw.edu>
2016-08-17 23:55:52 +0000

    add location search

--- instaSite/models.py
+++ instaSite/models.py
@@ -5,18 +5,17 @@ from django.db import models
 
 class User(models.Model):
 	username = models.CharField(max_length=100, null=True)
-	user = models.CharField(max_length=100, null=True)
 	accessToken = models.CharField(max_length=300, null=True)
 	accessSecret = models.CharField(max_length=300, null=True)
 
 	def __str__(self):
 		return self.username
-		user = models.ForeignKey(User, null=True)
 
 
 class Category(models.Model):
 	folder = models.CharField(max_length=100, null=True)
-
+	user = models.ForeignKey(User, null=True)
+	
 	def __str__(self):
 		return self.folder
 
--- instaSite/views.py
+++ instaSite/views.py
@@ -1,5 +1,4 @@
 import json 
-
 from django.conf import settings
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import redirect, render
@@ -9,9 +8,10 @@ from django.shortcuts import render_to_response
 from social.backends.oauth import BaseOAuth1, BaseOAuth2
 
 from django.apps import AppConfig
-from .models import Picture, Category
+from .models import Picture, Category, User
 from .forms import CategoryForm
 import tweepy
+from tweepy.auth import OAuthHandler
 import requests
 
 
@@ -20,22 +20,61 @@ from django.shortcuts import render_to_response, redirect
 from django.contrib.messages.api import get_messages
 from django.contrib.auth import views as auth_views
 
-from django.core.urlresolvers import reverse
+
+
+auth = tweepy.OAuthHandler(settings.SOCIAL_AUTH_TWITTER_KEY , settings.SOCIAL_AUTH_TWITTER_SECRET)
+session = {}
+access_token = ''
+access_token_secret = ''
+
+def auth(request):
+	auth_url = oauth.get_authorization_url(True)
+	response = HttpResponseDirect(auth_url)
+	session['request_token'] = auth.request_token 
+	return response
+
+def callback(request):
+	verifier = request.GET.get('oauth_verifier')
+	token = request['request_token']
+	acces_token = token
+	del_session['request_token']
+	auth.request_token = token
+	try:
+		auth.get_access_token(verifier)
+	except tweepy.TweepError:
+		print(error)
+	u = User(accessToken=auth.access_token,accessSecret = auth.access_token_secret)
+	session['access_token'] = auth.access_token
+	session['access_token_secret'] = auth.access_token_secret
+	u.save()
+	response = HttpResponseRedirect(reverse('/'))
+	return response
+
+def get_api(request):
+	
+	auth = tweepy.OAuthHandler(settings.SOCIAL_AUTH_TWITTER_KEY , settings.SOCIAL_AUTH_TWITTER_SECRET)
+	r_token = session.get('access_token')
+	r_secret = session.get('access_token_secret')
+	auth.set_access_token(r_token, r_secret)
+	api = tweepy.API(auth)
+	return api
+
 
 def logout(request):
 	"""Logs out user"""
 	auth_logout(request)
-	return redirect('/')
+	return render_to_response('instaSite/home.html', {})
 
 def create_user(self, email=None, password=None):
 	user.save()
 	return user 
 
-def home(request):
-	""" Home view, displays login mechanism"""
-	if request.user.is_authenticated():
-		return redirect('done')
-	return context()
+#
+##	def home(request):
+#	 Home view, displays login mechanism
+#	if request.user.is_authenticated():
+#		return redirect('done')
+#	return context()
 
 
 
@@ -79,12 +118,41 @@ def picture_list(request):
 	return render(request, 'instaSite/picture_list.html', {'pictures': pictures})
 
 
+
 def home(request):
-	context = RequestContext(request,{'request': request,'user': request.user})
+
+	context = RequestContext(request, {'request': request,'user': request.user})
+
 	return render_to_response('instaSite/home.html',context_instance=context)
 
+
+
 def location(request):
-	return render(request, 'instaSite/location.html', {})
+	api = get_api(request)
+	#for x in range(0, 3):
+	results = api.search('dog', count=100)
+	tweets_images=[]
+	for i in results:
+		try:
+			if i.entities['media'][0]['type']=='photo':
+				tweets_images.append({'url':i.entities['media'][0]['media_url'],'id':i.id})
+				id_images.append(i.id)
+		except:
+			pass
+	return render(request, 'instaSite/location.html', {'results': tweets_images})
+
+
+def picture_add(request):
+	if request.method == "POST":
+		form = PictureForm(request.POST)
+		if form.is_valid():
+			picture = form.save(commit=False)
+			category.folder = form.cleaned_data['picture']
+			category.save()
+			return redirect('location')
+		else:
+			form = PictureForm()
+	return render(request, 'instaSite/category_new.html', {'form': form})
 
 def main(request):
 	return render(request, 'instaSite/home.html', {})
